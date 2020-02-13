f3e78c47d515ca073d561af4a5a9672654c5a2bd
jonny rhea <jonathan.rhea@gmail.com>
2020-02-12 18:13:31 -0600

    mods made to debug ee logic natively

--- ee/src/main.rs
+++ ee/src/main.rs
@@ -9,11 +9,39 @@ use proof::{
     number::{Number, U4},
     reflist::RefList,
 };
+use oof::Oof;
+
+mod native {
+    extern "C" {
+        pub fn eth2_loadPreStateRoot(offset: *const u32);
+        pub fn eth2_blockDataSize() -> u32;
+        pub fn eth2_blockDataCopy(outputOfset: *const u32, offset: u32, length: u32);
+        pub fn eth2_savePostStateRoot(offset: *const u32);
+        pub fn print(output: *const u32, outputLength: u32);
+    }
+}
 
 #[no_mangle]
-pub extern "C" fn main() {}
+pub extern "C" fn main() {
+    let input_size = unsafe { native::eth2_blockDataSize() as usize };
 
-pub fn entry(blob: &mut [u8], pre: [u8; 32]) {
+    // Copy input into buffer
+    let mut input = [0u8; 64000];
+    unsafe {
+        native::eth2_blockDataCopy(input.as_mut_ptr() as *const u32, 0, input_size as u32);
+    }
+
+    // Get pre-state-root
+    let mut pre_state_root = [0u8; 32];
+    unsafe { native::eth2_loadPreStateRoot(pre_state_root.as_mut_ptr() as *const u32) }
+
+    // Process input data
+    let post_root = entry(&mut input, pre_state_root);
+    // Return post state
+    unsafe { native::eth2_savePostStateRoot(post_root.as_ptr() as *const u32) }
+}
+
+pub fn entry(blob: &mut [u8], pre: [u8; 32]) -> [u8; 32] {
     let mut blob = RawBlob::new(blob);
     let mut db = RefList::<RefAccount, U4>::from_raw(blob.raw_proof());
     db.verify(&pre);
@@ -21,13 +49,17 @@ pub fn entry(blob: &mut [u8], pre: [u8; 32]) {
     let txs = blob.transactions();
 
     for tx in txs {
-        db.begin();
+        process_tx(&mut db, &tx);
+       /* db.begin();
 
         match process_tx(&mut db, &tx) {
             OK => db.commit(),
             _ => db.rollback(),
-        }
+        }*/
     }
+    //*db.root().unwrap()
+    let root = [1u8; 32];
+    root
 }
 
 fn process_tx<N: Number>(db: &mut RefList<RefAccount, N>, tx: &Transaction) -> Error {
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,109 @@
 
 
-use wrapper::generate::ee_code;
+
+use wrapper::generate::{build_state, ee_code, transfer};
+use proof::number::U2;
+use oof::Oof;
 
 use interface::{
     error::{Error, OK},
     RawBlob, RefAccount, Transaction,
+    Account, Address, PublicKey, 
+    SIGNATURE_LEN
 };
 use proof::{
     number::{Number, U4},
     reflist::RefList,
+    reflist::RefNode
 };
+use std::fs;
+use arrayref::array_ref;
+extern crate byteorder;
+use byteorder::{ByteOrder, LittleEndian, WriteBytesExt};
 
 
-fn main() {
+/// This is the entry point when compiled as an executable binary.
+pub fn main() {
 
+
+    // build initial state
+    let mut one_pk = vec![0; 49];
+    let mut two_pk = vec![0; 49];
+    one_pk.extend(&((std::u128::MAX >> 1) - 2).to_le_bytes());
+    two_pk.extend(&((std::u128::MAX >> 1) + 1).to_le_bytes());
+
+    let mut zero = Account {
+        balance: 100,
+        nonce: 1,
+        pubkey: PublicKey::new(*array_ref![one_pk, 0, 65]),
+    };
+
+    let mut one = Account {
+        balance: 42,
+        nonce: 2,
+        pubkey: PublicKey::new(*array_ref![two_pk, 0, 65]),
+    };
+    let initial_state = build_state::<U2>(vec![zero.clone(), one.clone()]);
+    let mut initial_state_proof = build_state::<U2>(vec![zero.clone(), one.clone()]).to_proof();
+    let initial_state_root = initial_state_proof.root().unwrap();
+
+    let keys = initial_state_proof.keys();
+    println!("keys: {:?}",keys);
+
+    // create a tx
+    let tx = Transaction {
+        to: Address::zero(),
+        from: Address::one(),
+        nonce: 3,
+        amount: 1,
+        signature: [5; SIGNATURE_LEN],
+    }.to_bytes().to_vec();
+
+    let mut num_tx: Vec<u8> = Vec::new();
+    num_tx.write_u32::<LittleEndian>(1).unwrap();
+
+    let pre_state_root = initial_state_proof.root().unwrap();
+    //let post_state_root = hex::decode(args[1]).unwrap();
+    let mut input = [num_tx.to_vec(),tx,initial_state.to_proof().to_bytes()].concat();
+    // Process input data
+    let post_state_root = entry(&mut input, array_ref![pre_state_root, 0, 32]);
+
+    //assert_eq!(post_root, *array_ref![post_state_root, 0, 32]);
+
+    println!("pre_state_root  => {:?}", hex::encode(pre_state_root));
+    println!("post_state_root => {:?}", hex::encode(post_state_root));
+}
+
+pub fn entry(blob: &mut [u8], pre: &[u8; 32]) -> [u8; 32] {
+    let mut blob = RawBlob::new(blob);
+    let mut db = RefList::<RefAccount, U2>::from_raw(blob.raw_proof());
+    db.verify(&pre);
+
+    let txs = blob.transactions();
+
+    for tx in txs {
+        process_tx(&mut db, &tx);
+       /* db.begin();
+
+        match process_tx(&mut db, &tx) {
+            OK => db.commit(),
+            _ => db.rollback(),
+        }*/
+    }
+    *db.root().unwrap()
+}
+
+fn process_tx<N: Number>(db: &mut RefList<RefAccount, N>, tx: &Transaction) -> Error {
+    tx.verify();
+    let mut to = db.get_mut(tx.to.into());
+    let mut from = db.get_mut(tx.from.into());
+
+    let to_balance = to.balance();
+    let from_balance = from.balance();
+
+    to.set_balance(to_balance + tx.amount);
+    from.set_balance(from_balance - tx.amount);
+    from.inc_nonce();
+
+    OK
 }
